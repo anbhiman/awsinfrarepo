9d2566ba5a287dc902f4f7a163371881306f6552
anbhiman <40796080+anbhiman@users.noreply.github.com>
2022-11-28 13:55:30 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ backend "s3" {
     bucket = "mydev-tf-state-bucket"
     key = "terraform.tfstate"
     region = "us-east-1"
+    profile = "my-profile"
    
   }
 }
@@ -20,6 +21,7 @@ provider "aws" {
     aws_secret_access_key = "W4rjqUu/yvbwoJMyh8ULqLzq4YCLPhxrGZA6izND"
 
     region = "us-east-1"
+    profile = "my-profile"
 
 }
 resource "aws_s3_bucket" "b" {
